bcba62b1b00aacee93c099e8fbe67b85bc461909
Nazarii_Krutiak <nazariik20@gmail.com>
2023-12-20 02:11:13 +0200

    php_0_0_1

---
+++ views/site/index.php
@@ -0,0 +1,51 @@
+<?php
+
+/** @var yii\web\View $this */
+
+$this->title = 'My Yii Application';
+?>
+<div class="site-index">
+
+    <div class="jumbotron text-center bg-transparent mt-5 mb-5">
+        <h1 class="display-4">Дякую< що дали змогу порівняти Php, та JS</h1>
+
+                <p class="lead">Нажаль я не встиг пройти все до кінця, але зберіг відео.</p>
+                <p class="lead" style="text-align: right;">Над роботою працював вчитель інформатики Крутяк Н. О. </p>
+                <p class="lead" style="text-align: right;">Нище розміщу інформацію про захоплення</p>
+    </div>
+
+    <div class="body-content">
+
+        <div class="row">
+            <div class="col-lg-4 mb-3">
+                <h2>Бази даних</h2>
+
+                <p>Оскільки використовую JS для розробки, то мені більше імпонують БД по типу NoSQL. Вони надзвичайно
+                    зручні,як на мене</p>
+
+                <p><a class="btn btn-outline-secondary" href="https://www.mongodb.com/">Переглянути
+                        &raquo;</a></p>
+            </div>
+            <div class="col-lg-4 mb-3">
+                <h2>React</h2>
+
+                <p>Основною перевагою React є велика кількість документації та підтримки(комюніті). Також, щодо
+                    швидкості нам генерується фактично одна сторінка. Ну і беручи до уваги Redux та велику кількість
+                    хукі розробка проходить доволі швидко.(правда в моноліті швидше,як побачив) </p>
+
+                <p><a class="btn btn-outline-secondary" href="https://ru.legacy.reactjs.org/">Переглянути &raquo;</a>
+                </p>
+            </div>
+            <div class="col-lg-4">
+                <h2>Docker та серверна частина в цілому</h2>
+
+                <p>Шукаю наразі додаткову інформацію по серверній частині. Поки в ци питаннях профан. Замітив, що yui
+                    використовує Docker, поки напряму з ним не працював, але думаю варто</p>
+
+                <p><a class="btn btn-outline-secondary" href="https://www.docker.com/">Docker
+                        &raquo;</a></p>
+            </div>
+        </div>
+
+    </div>
+</div>
